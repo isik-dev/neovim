5482c9d3547072f342ad5465af473d21f68ef214
arceus <islom4ik.numanov@gmail.com>
2026-02-20 11:44:17 +0900

    chore: fixed speed degradation in m4, caused by pyright watching all files

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,31 +1,46 @@
-return {
-	{
-		"mason-org/mason.nvim",
-		opts = {}
-	},
-	{
-		"mason-org/mason-lspconfig.nvim",
-		opts = {
-			ensure_installed = {"pyright"},
-		},
-		dependencies = {
-			{ "mason-org/mason.nvim", opts = {} },
-			"neovim/nvim-lspconfig",
-		},
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			-- require("lspconfig").pyright.setup({}) -- this is deprecated
-			vim.lsp.config("pyright", {})
-			vim.lsp.enable("pyright")
-      			vim.api.nvim_create_autocmd("LspAttach", {
-      			    callback = function(args)
-      			        vim.keymap.set("n", "gd", vim.lsp.buf.definition, {
-					buffer = args.buf })
-      			    end,
-      			})
+  return {
+        {
+                "mason-org/mason.nvim",
+                opts = {}
+        },
+        {
+                "mason-org/mason-lspconfig.nvim",
+                opts = {
+                        ensure_installed = {"pyright"},
+                },
+                dependencies = {
+                        { "mason-org/mason.nvim", opts = {} },
+                        "neovim/nvim-lspconfig",
+                },
+        },
+        {
+                "neovim/nvim-lspconfig",
+                ft = "python",  -- Only load when opening Python files
+                config = function()
+                        -- Disable file watching capability to prevent performance issues
+                        local capabilities = vim.lsp.protocol.make_client_capabilities()
+                        capabilities.workspace.didChangeWatchedFiles.dynamicRegistration = false
 
-		end,
-	},
-}
+                        vim.lsp.config("pyright", {
+                                capabilities = capabilities,
+                                settings = {
+                                        python = {
+                                                analysis = {
+                                                        autoSearchPaths = true,
+                                                        diagnosticMode = "openFilesOnly",  -- Don't analyze entire workspace
+                                                        useLibraryCodeForTypes = true,
+                                                }
+                                        }
+                                }
+                        })
+
+                        vim.api.nvim_create_autocmd("LspAttach", {
+                                callback = function(args)
+                                        vim.keymap.set("n", "gd", vim.lsp.buf.definition, {
+                                                buffer = args.buf
+                                        })
+                                end,
+                        })
+                end,
+        },
+  }
