f429b2208e4a5fd49bd9049e3899f46bded33b5f
arceus <islom4ik.numanov@gmail.com>
2026-02-22 21:41:36 +0900

    chore: added a keymap for exiting command mode & keymap for to open botright terminal from inside the neovim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,5 @@
 vim.keymap.set("i", "jk", "<ESC>")
+vim.keymap.set("c", "jk", "<ESC>")
 vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files
 vim.keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- search text
+vim.keymap.set("n", "<leader>i", "<cmd>botright term<cr>") -- open bottom terminal
